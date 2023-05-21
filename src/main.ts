e918d7dedc9355dc4b6ce2bfe99318371b637d3e
cesarhro <cesarhr.olimpio@gmail.com>
2023-05-21 19:59:42 -0300

    feature: Adicionando https na API

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
+
+const httpsOptions = {
+  key: fs.readFileSync('./secrets/private-key.pem'),
+  cert: fs.readFileSync('./secrets/public-certificate.pem'),
+};
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   await app.listen(3000);
 }
 bootstrap();
