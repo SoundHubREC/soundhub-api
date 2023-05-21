1bdfa4a78cfa634bd7fa121330f59ea0c0df9625
cesarhro <cesarhr.olimpio@gmail.com>
2023-05-21 20:58:35 -0300

    chore: Removendo https :(

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as fs from 'fs';
-
-const httpsOptions = {
-  key: fs.readFileSync('./secrets/private-key.pem'),
-  cert: fs.readFileSync('./secrets/public-certificate.pem'),
-};
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { httpsOptions });
+  const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
 bootstrap();
