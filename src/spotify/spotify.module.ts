29e8e7dab197584135dba1e2d77e52b5ad4da809
cesarhro <cesarhr.olimpio@gmail.com>
2023-05-28 10:45:43 -0300

    fix: Removendo modulos não utilizados

--- src/spotify/spotify.module.ts
+++ src/spotify/spotify.module.ts
@@ -2,13 +2,8 @@ import { Module } from '@nestjs/common';
 import { TracksController } from './spotify.controller';
 import { SpotifyService } from './spotify.service';
 import { AuthService } from './auth.service';
-import { MongooseModule } from '@nestjs/mongoose';
-import { TracksSchema } from './schemas/tracks.schema';
 
 @Module({
-  imports: [
-    MongooseModule.forFeature([{ name: 'Tracks', schema: TracksSchema }]),
-  ],
   controllers: [TracksController],
   providers: [SpotifyService, AuthService],
 })
